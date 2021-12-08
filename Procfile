ebbad8608b3f777e517b06154e20e3f3fa99d723
Jeffrey <jrgilley92@gmail.com>
2021-12-08 15:54:13 -0500

    added Procfile

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: gunicorn django_library_api.wsgi
+release: python manage.py migrate
--- django_library_api/settings.py
+++ django_library_api/settings.py
@@ -153,3 +153,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 15,
 }
+
+# Django-on-heroku
+
+import django_on_heroku
+django_on_heroku.settings(locals())
+del DATABASES['default']['OPTIONS']['sslmode']
